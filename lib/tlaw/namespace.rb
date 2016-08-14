9278b9ca0f31668212ef748b00dc3cfbc4cff599
zverok <zverok.offline@gmail.com>
2016-08-14 21:14:05 +0300

    Now, namespaces, for realz

---
+++ lib/tlaw/namespace.rb
@@ -0,0 +1,32 @@
+module TLAW
+  class Namespace
+    attr_reader :endpoints
+
+    def initialize(api)
+      @api = api
+      @endpoints = self.class.endpoints.map { |name, klass| [name, klass.new(api)] }.to_h
+    end
+
+    class << self
+      attr_accessor :api
+      attr_accessor :path
+      attr_accessor :namespace_name
+
+      def add_endpoint(endpoint)
+        name = endpoint.endpoint_name
+
+        # TODO: validate a) if it already exists b) if it is classifiable
+        const_set(Util::camelize(name), endpoint)
+        endpoints[name] = endpoint
+
+        define_method(name) { |*arg, **param|
+          @endpoints[name].call(*arg, **{_namespace: self.class.path}.merge(param))
+        }
+      end
+
+      def endpoints
+        @endpoints ||= {}
+      end
+    end
+  end
+end
---
+++ spec/tlaw/namespace_spec.rb
@@ -0,0 +1,52 @@
+module TLAW
+  describe API do
+    context 'definition' do
+      describe '.define' do
+      end
+
+      describe '.add_param' do
+      end
+
+      describe '.add_endpoint' do
+        let(:endpoint) { Class.new(Endpoint) }
+        subject(:namespace) { Class.new(Namespace) }
+        before {
+          endpoint.endpoint_name = :some_endpoint
+          namespace.__send__(:add_endpoint, endpoint)
+        }
+
+        its(:constants) { is_expected.to include(:SomeEndpoint) }
+        its(:instance_methods) { is_expected.to include(:some_endpoint) }
+        its(:endpoints) { is_expected.to include(some_endpoint: endpoint) }
+      end
+    end
+
+    context 'instance' do
+      let(:endpoint_class) { Class.new(Endpoint) { self.endpoint_name = :some_ep } }
+
+      let!(:namespace_class) {
+        Class.new(Namespace).tap { |c|
+          c.path = 'ns'
+          c.add_endpoint endpoint_class
+        }
+      }
+
+      let(:api) { instance_double('TLAW::API') }
+
+      subject(:namespace) { namespace_class.new(api)  }
+
+      describe '#initialize' do
+        it 'instantiates endpoints' do
+          expect(namespace.endpoints[:some_ep]).to be_an Endpoint
+        end
+      end
+
+      describe '#<endpoint>' do
+        it 'calls proper endpoint' do
+          expect(namespace.endpoints[:some_ep]).to receive(:call).with(foo: 'bar', _namespace: 'ns')
+          namespace.some_ep(foo: 'bar')
+        end
+      end
+    end
+  end
+end
