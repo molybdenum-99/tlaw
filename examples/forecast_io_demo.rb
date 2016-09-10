9860c0e4d5d90c6f714d871dc43723c685492d0f
zverok <zverok.offline@gmail.com>
2016-09-10 15:54:50 +0300

    Fix param name for demo

--- examples/forecast_io_demo.rb
+++ examples/forecast_io_demo.rb
@@ -5,7 +5,7 @@ require_relative 'forecast_io'
 weather = TLAW::Examples::ForecastIO
   .new(api_key: ENV['FORECAST_IO'], units: :si)
 
-pp weather.forecast(49.999892, 36.242392, hourly: true)
+pp weather.forecast(49.999892, 36.242392, extended_hourly: true)
 
 
 res = weather.time_machine(49.999892, 36.242392, Date.parse('2011-02-01'))
