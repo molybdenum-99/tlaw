6b7ce09464ce9cfebd56dd814ce4705904233f29
zverok <zverok.offline@gmail.com>
2016-09-13 23:32:17 +0300

    More self-promotion in UD example

--- examples/urbandictionary_demo.rb
+++ examples/urbandictionary_demo.rb
@@ -17,6 +17,15 @@ require_relative 'demo_base'
 # what you get when you need to design everything from scratch, like HTTP
 # client and params processing and response parsing and whatnot.
 #
+# Oh, and there is another one: https://github.com/tmiller/urban
+#
+# But when somebody really need them (for chatbots), they use neither,
+# just redefine everything from scratch with rough net connection and
+# response parsing (because both of aforementioned gems are too thick
+# wrappers to rely on them):
+# * https://github.com/jimmycuadra/lita-urban-dictionary
+# * https://github.com/cinchrb/cinch-urbandictionary
+#
 # Here is our version (17 codelines, including namespacing and bit of docs):
 #
 module TLAW
@@ -40,6 +49,8 @@ module TLAW
   end
 end
 
+# Usage (response is clear as tears, could be integrated anywhere):
+
 d = TLAW::Examples::UrbanDictionary.new
 
 p d.describe
@@ -47,13 +58,11 @@ p d.describe
 #   Really small API. Described as "official but undocumented"
 #   by some.
 #
-#
 #   Endpoints:
 #
 #   .define(term)
 #
 #   .random()
-
 
 res = d.define('trollface')
 
@@ -75,21 +84,21 @@ pp res
 #  "sounds"=>[]}
 
 pp res['list'].columns('word', 'thumbs_up', 'thumbs_down').to_a
-[{"word"=>"Trollface", "thumbs_up"=>418, "thumbs_down"=>99},
- {"word"=>"Troll Face", "thumbs_up"=>481, "thumbs_down"=>318},
- {"word"=>"Troll Face", "thumbs_up"=>340, "thumbs_down"=>184},
- {"word"=>"trollface", "thumbs_up"=>115, "thumbs_down"=>35},
- {"word"=>"trollface", "thumbs_up"=>94, "thumbs_down"=>30},
- {"word"=>"trollface", "thumbs_up"=>61, "thumbs_down"=>54},
- {"word"=>"Troll Face", "thumbs_up"=>81, "thumbs_down"=>181}]
+# [{"word"=>"Trollface", "thumbs_up"=>418, "thumbs_down"=>99},
+#  {"word"=>"Troll Face", "thumbs_up"=>481, "thumbs_down"=>318},
+#  {"word"=>"Troll Face", "thumbs_up"=>340, "thumbs_down"=>184},
+#  {"word"=>"trollface", "thumbs_up"=>115, "thumbs_down"=>35},
+#  {"word"=>"trollface", "thumbs_up"=>94, "thumbs_down"=>30},
+#  {"word"=>"trollface", "thumbs_up"=>61, "thumbs_down"=>54},
+#  {"word"=>"Troll Face", "thumbs_up"=>81, "thumbs_down"=>181}]
 
 pp d.random['list'].columns('word', 'example').first(3).to_a
-[{"word"=>"pH", "example"=>"Get that ph out of the shower.\r\n\r\n "},
- {"word"=>"spocking",
-  "example"=>
-   "The dirty bitch couldn’t get enough so I gave her a damn good spocking"},
- {"word"=>"mormon",
-  "example"=>
-   "Oh my gosh are the Smiths mormons?! We better have a party so they can bring some frickin' sweet green jello!..."}]
+# [{"word"=>"pH", "example"=>"Get that ph out of the shower.\r\n\r\n "},
+#  {"word"=>"spocking",
+#   "example"=>
+#    "The dirty bitch couldn’t get enough so I gave her a damn good spocking"},
+#  {"word"=>"mormon",
+#   "example"=>
+#    "Oh my gosh are the Smiths mormons?! We better have a party so they can bring some frickin' sweet green jello!..."}]
 
 # That's it  ¯\_(ツ)_/¯
