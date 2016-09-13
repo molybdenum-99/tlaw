8c6a81fe4d305fa20bab1d065120d529c1f2b755
zverok <zverok.offline@gmail.com>
2016-09-13 22:25:31 +0300

    Remove wunderground for now... Just lazy

--- examples/wunderground_demo.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-require_relative 'demo_base'
-require_relative 'wunderground'
-
-weather = TLAW::Examples::WUnderground.new(api_key: ENV['WUNDERGROUND'])
-
-pp weather.city('Kharkiv', 'Ukraine', features: %i[astronomy tide])
