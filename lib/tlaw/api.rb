e7a2db3bbdb765067414b67df457e8d9da85992e
zverok <zverok.offline@gmail.com>
2016-09-14 22:46:00 +0300

    More YARD

--- lib/tlaw/api.rb
+++ lib/tlaw/api.rb
@@ -1,13 +1,55 @@
 module TLAW
+  # API is just a top-level {Namespace}.
+  #
+  # Basically, you start creating your endpoint by descending from API
+  # and defining namespaces and endpoints through a {DSL} like this:
+  #
+  # ```ruby
+  # class MyCoolAPI < TLAW::API
+  #   define do
+  #     base 'http://api.mycool.com'
+  #
+  #     namespace :awesome do
+  #       # ...and so on
+  #     end
+  #   end
+  # end
+  # ```
+  #
+  # And then, you use it:
+  #
+  # ```ruby
+  # api = MyCoolAPI.new
+  # api.awesome.cool(param: 'value')
+  # ```
+  #
+  # See {DSL} for explanation of API definition, {Namespace} for explanation
+  # of possible usages and {Endpoint} for real calls performing.
+  #
   class API < Namespace
+    # Thrown when there are an error during call. Contains real URL which
+    # was called at the time of an error.
     class Error < RuntimeError
     end
 
     class << self
+      # Runs the {DSL} inside your API wrapper class.
       def define(&block)
         DSL::APIWrapper.new(self).define(&block)
       end
 
+      # Returns detailed description of an API, like this:
+      #
+      # ```ruby
+      # MyCoolAPI.describe
+      # # MyCoolAPI.new()
+      # #   This is cool API.
+      # #
+      # #   Namespaces:
+      # #   .awesome()
+      # #     This is awesome.
+      # ```
+      #
       def describe(*)
         super.sub(/\A./, '')
       end
--- lib/tlaw/data_table.rb
+++ lib/tlaw/data_table.rb
@@ -1,4 +1,29 @@
 module TLAW
+  # Basically, just a 2-d array with column names. Or you can think of
+  # it as an array of hashes. Or loose DataFrame implementation.
+  #
+  # Just like this:
+  #
+  # ```ruby
+  # tbl = DataTable.new([
+  #   {id: 1, name: 'Mike', salary: 1000},
+  #   {id: 2, name: 'Doris', salary: 900},
+  #   {id: 3, name: 'Angie', salary: 1200}
+  # ])
+  # # => #<TLAW::DataTable[id, name, salary] x 3>
+  # tbl.count
+  # # => 3
+  # tbl.keys
+  # # => ["id", "name", "salary"]
+  # tbl[0]
+  # # => {"id"=>1, "name"=>"Mike", "salary"=>1000}
+  # tbl['salary']
+  # # => [1000, 900, 1200]
+  # ```
+  #
+  # Basically, that's it. Every array of hashes in TLAW response will be
+  # converted into corresponding `DataTable`.
+  #
   class DataTable < Array
     def self.from_columns(column_names, columns)
       from_rows(column_names, columns.transpose)
@@ -8,6 +33,12 @@ module TLAW
       new rows.map { |r| column_names.zip(r).to_h }
     end
 
+    # Creates DataTable from array of hashes.
+    #
+    # Note, that all hash keys are stringified, and all hashes are expanded
+    # to have same set of keys.
+    #
+    # @param hashes [Array<Hash>]
     def initialize(hashes)
       hashes = hashes.each_with_index.map { |h, i|
         h.is_a?(Hash) or
@@ -21,10 +52,27 @@ module TLAW
       super(hashes)
     end
 
+    # All column names.
+    #
+    # @return [Array<String>]
     def keys
       empty? ? [] : first.keys
     end
 
+    # Allows access to one column or row.
+    #
+    # @overload [](index)
+    #   Returns one row from a DataTable.
+    #
+    #   @param index [Integer] Row number
+    #   @return [Hash] Row as a hash
+    #
+    # @overload [](column_name)
+    #   Returns one column from a DataTable.
+    #
+    #   @param column_name [String] Name of column
+    #   @return [Array] Column as an array of all values in it
+    #
     def [](index_or_column)
       case index_or_column
       when Integer
@@ -38,19 +86,29 @@ module TLAW
       end
     end
 
+    # Slice of a DataTable with only specified columns left.
+    #
+    # @param names [Array<String>] What columns to leave in a DataTable
+    # @return [DataTable]
     def columns(*names)
       names.map!(&:to_s)
       DataTable.new(map { |h| names.map { |n| [n, h[n]] }.to_h })
     end
 
+    # Represents DataTable as a `column name => all values in columns`
+    # hash.
+    #
+    # @return [Hash{String => Array}]
     def to_h
       keys.map { |k| [k, map { |h| h[k] }] }.to_h
     end
 
+    # @private
     def inspect
       "#<#{self.class.name}[#{keys.join(', ')}] x #{size}>"
     end
 
+    # @private
     def pretty_print(pp)
       pp.text("#<#{self.class.name}[#{keys.join(', ')}] x #{size}>")
     end
